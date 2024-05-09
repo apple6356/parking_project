cda72d92328590d2c3697b68621a8605cb17a154
apple6356 <79889755+apple6356@users.noreply.github.com>
2024-05-09 22:40:57 +0900

    Add files via upload

---
+++ 주차관리프로그램/Parking_project/CDlgDayAC.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+
+// CDlgDayAC 대화 상자
+
+class CDlgDayAC : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgDayAC)
+
+public:
+	CDlgDayAC(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgDayAC();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_AC };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strMon;
+	CString m_strTue;
+	CString m_strWed;
+	CString m_strThur;
+	CString m_strFri;
+	CString m_strSat;
+	CString m_strSun;
+	afx_msg void OnClose();
+};
