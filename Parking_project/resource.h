b246437c42f2637cbcd696e5f9179e5ade71b485
apple6356 <79889755+apple6356@users.noreply.github.com>
2024-05-09 23:28:05 +0900

    Add files via upload

---
+++ Parking_project/resource.h
@@ -0,0 +1,41 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Parkingproject.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PARKING_PROJECT_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_AC                   131
+#define IDC_BUTTON1                     1000
+#define IDC_LIST2                       1002
+#define IDC_LIST_PARKING                1002
+#define IDC_BUTTON_IN                   1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT_INNUM                  1004
+#define IDC_EDIT_MONDAY                 1004
+#define IDC_BUTTON_OUT                  1005
+#define IDC_EDIT_TUESDAY                1005
+#define IDC_EDIT2                       1006
+#define IDC_EDIT_MONEY                  1006
+#define IDC_EDIT_WEDNESDAY              1006
+#define IDC_BUTTON_MONEY                1007
+#define IDC_EDIT_THURSDAY               1007
+#define IDC_EDIT_OUTNUM                 1008
+#define IDC_EDIT_FRIDAY                 1008
+#define IDC_EDIT_SATURDAY               1009
+#define IDC_EDIT_SUNDAY                 1010
+#define IDC_EDIT_TODAY                  1011
+#define IDC_PARKING                     1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
